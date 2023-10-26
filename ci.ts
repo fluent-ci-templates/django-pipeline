5b08e8295d9b8804d3118ce8b8ea0da211dc21ef
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-26 05:32:03 +0000

    update deno.lock

--- ci.ts
+++ ci.ts
@@ -1,3 +1,3 @@
-import { djangoTests } from "https://pkg.fluentci.io/django_pipeline@v0.6.0/mod.ts";
+import { djangoTests } from "https://pkg.fluentci.io/django_pipeline@v0.6.1/mod.ts";
 
 await djangoTests();
