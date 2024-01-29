a6b41b7b817ad596fa8c99acd5018e212ddbd40d
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-29 05:57:46 +0000

    fix issue with dagger connect
    fix imports

--- example/.fluentci/src/dagger/jobs.ts
+++ example/.fluentci/src/dagger/jobs.ts
@@ -1,5 +1,4 @@
-import { Client, Directory } from "../../sdk/client.gen.ts";
-import { connect } from "../../sdk/connect.ts";
+import { Directory, dag } from "../../sdk/client.gen.ts";
 import { getDirectory } from "./lib.ts";
 
 export enum Job {
@@ -17,68 +16,66 @@ export const exclude = [".git", ".devbox", ".fluentci"];
 export async function djangoTests(
   src: Directory | string | undefined = "."
 ): Promise<string> {
-  let result = "";
-  await connect(async (client: Client) => {
-    // get MariaDB base image
-    const mariadb = client
-      .container()
-      .from("mariadb:10.11.2")
-      .withEnvVariable("MARIADB_USER", Deno.env.get("MARIADB_USER") || "user")
-      .withEnvVariable(
-        "MARIADB_PASSWORD",
-        Deno.env.get("MARIADB_PASSWORD") || "password"
-      )
-      .withEnvVariable("MARIADB_DATABASE", "test_db")
-      .withEnvVariable(
-        "MARIADB_ROOT_PASSWORD",
-        Deno.env.get("MARIADB_ROOT_PASSWORD") || "root"
-      )
-      .withExposedPort(3306)
-      .asService();
+  // get MariaDB base image
+  const mariadb = dag
+    .container()
+    .from("mariadb:10.11.2")
+    .withEnvVariable("MARIADB_USER", Deno.env.get("MARIADB_USER") || "user")
+    .withEnvVariable(
+      "MARIADB_PASSWORD",
+      Deno.env.get("MARIADB_PASSWORD") || "password"
+    )
+    .withEnvVariable("MARIADB_DATABASE", "test_db")
+    .withEnvVariable(
+      "MARIADB_ROOT_PASSWORD",
+      Deno.env.get("MARIADB_ROOT_PASSWORD") || "root"
+    )
+    .withExposedPort(3306)
+    .asService();
 
-    const context = getDirectory(client, src);
-    const baseCtr = client
-      .pipeline(Job.djangoTests)
-      .container()
-      .from("ghcr.io/fluentci-io/pkgx:latest")
-      .withExec(["apt-get", "update"])
-      .withExec([
-        "apt-get",
-        "install",
-        "-y",
-        "build-essential",
-        "python3-dev",
-        "default-libmysqlclient-dev",
-        "pkg-config",
-        "python3-pip",
-        "python3.11-venv",
-        "python3-full",
-      ]);
+  const context = await getDirectory(dag, src);
+  const baseCtr = dag
+    .pipeline(Job.djangoTests)
+    .container()
+    .from("ghcr.io/fluentci-io/pkgx:latest")
+    .withExec(["apt-get", "update"])
+    .withExec([
+      "apt-get",
+      "install",
+      "-y",
+      "build-essential",
+      "python3-dev",
+      "default-libmysqlclient-dev",
+      "pkg-config",
+      "python3-pip",
+      "python3.11-venv",
+      "python3-full",
+    ]);
 
-    const ctr = baseCtr
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withServiceBinding("db", mariadb)
-      .withEnvVariable("MARIADB_USER", Deno.env.get("MARIADB_USER") || "user")
-      .withEnvVariable(
-        "MARIADB_PASSWORD",
-        Deno.env.get("MARIADB_PASSWORD") || "password"
-      )
-      .withEnvVariable("MARIADB_DATABASE", "test_db")
-      .withEnvVariable(
-        "MARIADB_ROOT_PASSWORD",
-        Deno.env.get("MARIADB_ROOT_PASSWORD") || "root"
-      )
-      .withEnvVariable("MARIADB_HOST", Deno.env.get("MARIADB_HOST") || "db")
-      .withEnvVariable("MYSQLCLIENT_CFLAGS", "-I/usr/include/mysql")
-      .withEnvVariable(
-        "MYSQLCLIENT_LDFLAGS",
-        "-L/usr/lib/x86_64-linux-gnu -lmysqlclient"
-      )
-      .withExec([
-        "bash",
-        "-c",
-        "\
+  const ctr = baseCtr
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withServiceBinding("db", mariadb)
+    .withEnvVariable("MARIADB_USER", Deno.env.get("MARIADB_USER") || "user")
+    .withEnvVariable(
+      "MARIADB_PASSWORD",
+      Deno.env.get("MARIADB_PASSWORD") || "password"
+    )
+    .withEnvVariable("MARIADB_DATABASE", "test_db")
+    .withEnvVariable(
+      "MARIADB_ROOT_PASSWORD",
+      Deno.env.get("MARIADB_ROOT_PASSWORD") || "root"
+    )
+    .withEnvVariable("MARIADB_HOST", Deno.env.get("MARIADB_HOST") || "db")
+    .withEnvVariable("MYSQLCLIENT_CFLAGS", "-I/usr/include/mysql")
+    .withEnvVariable(
+      "MYSQLCLIENT_LDFLAGS",
+      "-L/usr/lib/x86_64-linux-gnu -lmysqlclient"
+    )
+    .withExec([
+      "bash",
+      "-c",
+      "\
   python3 -m venv venv && \
   chmod a+x venv/bin/activate && \
   ls -ltr venv/bin && \
@@ -89,13 +86,12 @@ export async function djangoTests(
    python3 manage.py migrate && \
    python3 manage.py check && \
    python3 manage.py test",
-      ]);
-    result = await ctr.stdout();
-  });
+    ]);
+  const result = await ctr.stdout();
   return result;
 }
 
-export type JobExec = (src?: Directory | string) => Promise<string>;
+export type JobExec = (src?: string) => Promise<string>;
 
 export const runnableJobs: Record<Job, JobExec> = {
   [Job.djangoTests]: djangoTests,
